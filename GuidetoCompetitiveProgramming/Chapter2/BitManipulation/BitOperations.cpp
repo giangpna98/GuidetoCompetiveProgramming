50b9225025f29b257aff3acf19bf54d785ecc0db
giangpna98 <giangpna98@gmail.com>
2025-03-21 00:01:45 +0700

    Added RecursiveAlgorithms as submodule

---
+++ GuidetoCompetitiveProgramming/Chapter2/BitManipulation/BitOperations.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=31; i>=0; i--)
+    {
+        if(n & (1<<i)) cout<<"1";
+        else cout<<"0";
+    }
+    return 0;
+}
