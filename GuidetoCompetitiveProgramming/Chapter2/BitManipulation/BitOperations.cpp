50e1495843ec50cc4599d9cfd909280f48a3f031
giangpna98 <giangpna98@gmail.com>
2025-03-21 10:44:55 +0700

    Updated file

--- GuidetoCompetitiveProgramming/Chapter2/BitManipulation/BitOperations.cpp
+++ GuidetoCompetitiveProgramming/Chapter2/BitManipulation/BitOperations.cpp
@@ -10,5 +10,9 @@ int main(){
         if(n & (1<<i)) cout<<"1";
         else cout<<"0";
     }
+    cout<<__builtin_clz(n)<<"\n";
+    cout<<__builtin_ctz(n)<<"\n";
+    cout<<__builtin_popcount(n)<<"\n";
+    cout<<__builtin_parity(n)<<"\n";
     return 0;
 }
